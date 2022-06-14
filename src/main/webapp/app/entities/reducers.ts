68993f728c4a2fa3082e0600752011a460a99cf8
ybchoo <chooyb@gmail.com>
2022-06-14 20:47:16 +0800

    T

--- src/main/webapp/app/entities/reducers.ts
+++ src/main/webapp/app/entities/reducers.ts
@@ -5,6 +5,17 @@ import productOrder from 'app/entities/product-order/product-order.reducer';
 import orderItem from 'app/entities/order-item/order-item.reducer';
 import invoice from 'app/entities/invoice/invoice.reducer';
 import shipment from 'app/entities/shipment/shipment.reducer';
+import blog from 'app/entities/blog/blog.reducer';
+import entry from 'app/entities/entry/entry.reducer';
+import tag from 'app/entities/tag/tag.reducer';
+import department from 'app/entities/department/department.reducer';
+import jobHistory from 'app/entities/job-history/job-history.reducer';
+import job from 'app/entities/job/job.reducer';
+import employee from 'app/entities/employee/employee.reducer';
+import location from 'app/entities/location/location.reducer';
+import task from 'app/entities/task/task.reducer';
+import country from 'app/entities/country/country.reducer';
+import region from 'app/entities/region/region.reducer';
 /* jhipster-needle-add-reducer-import - JHipster will add reducer here */
 
 const entitiesReducers = {
@@ -15,6 +26,17 @@ const entitiesReducers = {
   orderItem,
   invoice,
   shipment,
+  blog,
+  entry,
+  tag,
+  department,
+  jobHistory,
+  job,
+  employee,
+  location,
+  task,
+  country,
+  region,
   /* jhipster-needle-add-reducer-combine - JHipster will add reducer here */
 };
 
--- src/main/webapp/app/entities/routes.tsx
+++ src/main/webapp/app/entities/routes.tsx
@@ -9,6 +9,17 @@ import ProductOrder from './product-order';
 import OrderItem from './order-item';
 import Invoice from './invoice';
 import Shipment from './shipment';
+import Blog from './blog';
+import Entry from './entry';
+import Tag from './tag';
+import Department from './department';
+import JobHistory from './job-history';
+import Job from './job';
+import Employee from './employee';
+import Location from './location';
+import Task from './task';
+import Country from './country';
+import Region from './region';
 /* jhipster-needle-add-route-import - JHipster will add routes here */
 
 export default ({ match }) => {
@@ -23,6 +34,17 @@ export default ({ match }) => {
         <ErrorBoundaryRoute path={`${match.url}order-item`} component={OrderItem} />
         <ErrorBoundaryRoute path={`${match.url}invoice`} component={Invoice} />
         <ErrorBoundaryRoute path={`${match.url}shipment`} component={Shipment} />
+        <ErrorBoundaryRoute path={`${match.url}blog`} component={Blog} />
+        <ErrorBoundaryRoute path={`${match.url}entry`} component={Entry} />
+        <ErrorBoundaryRoute path={`${match.url}tag`} component={Tag} />
+        <ErrorBoundaryRoute path={`${match.url}department`} component={Department} />
+        <ErrorBoundaryRoute path={`${match.url}job-history`} component={JobHistory} />
+        <ErrorBoundaryRoute path={`${match.url}job`} component={Job} />
+        <ErrorBoundaryRoute path={`${match.url}employee`} component={Employee} />
+        <ErrorBoundaryRoute path={`${match.url}location`} component={Location} />
+        <ErrorBoundaryRoute path={`${match.url}task`} component={Task} />
+        <ErrorBoundaryRoute path={`${match.url}country`} component={Country} />
+        <ErrorBoundaryRoute path={`${match.url}region`} component={Region} />
         {/* jhipster-needle-add-route-path - JHipster will add routes here */}
       </Switch>
     </div>
